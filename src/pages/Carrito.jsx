b5937a0aa7446a7ffb7f83b3704744968c4b0032
Alejandro Navarro <drojannp2@gmail.com>
2025-07-26 20:22:25 +0200

    feat: send order to backend with token auth and validation

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -7,6 +7,7 @@ import ErrorMessage from "../components/ErrorMessage"
 
 export const Carrito = () => {
     const MAPS_API = import.meta.env.VITE_MAPS_API;
+    const BACKEND_API = import.meta.env.VITE_BACKEND_API;
 
     const { orderProducts, removeFromOrder, clearOrder } = useContext(OrderContext);
     const { isLoggedIn, user } = useContext(AuthContext)
@@ -15,19 +16,11 @@ export const Carrito = () => {
 
     const existsAddress = user?.deliveryAddress ? true : false;
     const [address, setAddress] = useState(existsAddress ? user.deliveryAddress : "")
+    const [extraInformation, setExtraInformation] = useState("");
     const inputRef = useRef(null);
     const [error, setError] = useState(null)
+    const [selectedPayment, setSelectedPayment] = useState("tarjeta");
 
-
-
-    if (orderProducts.length === 0) {
-        return (
-            <div className="cart-page">
-                <h2>Tu carrito está vacío</h2>
-                <button onClick={() => navigate("/carta")} className="show-menu">Ver carta</button>
-            </div>
-        );
-    }
 
     useEffect(() => {
         if (document.querySelector('script[src*="maps.googleapis.com/maps/api/js"]')) {
@@ -80,6 +73,52 @@ export const Carrito = () => {
             }
         });
     };
+    const handlePayment = (method) => {
+        setSelectedPayment(method);
+    }
+
+    const submitOrder = async () => {
+        try {
+            const token = localStorage.getItem("token");
+            const response = await fetch(`${BACKEND_API}/orders`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    products: orderProducts.map((item) => ({
+                        id: item._id,
+                        quantity: 1,
+                        imageUrl: item.imageUrl
+                    })),
+                    deliveryAddress: `${address} ${extraInformation}`,
+                    selectedPayment: selectedPayment,
+                }),
+            });
+            const data = await response.json();
+
+            if (!response.ok || data.status === "error") {
+                throw new Error(data.msg);
+            }
+
+            console.log("Pedido enviado correctamente!");
+            clearOrder();
+            navigate("/perfil")
+        } catch (error) {
+            setError(error.message)
+        }
+    };
+
+
+    if (orderProducts.length === 0) {
+        return (
+            <div className="cart-page">
+                <h2>Tu carrito está vacío</h2>
+                <button onClick={() => navigate("/carta")} className="show-menu">Ver carta</button>
+            </div>
+        );
+    }
     return (
         <div className="cart-page">
             <div className="blob blob1"></div>
@@ -91,13 +130,28 @@ export const Carrito = () => {
                 ))}
             </ul>
             <h2 className="total-cart">Total: {total.toFixed(2)}€</h2>
-            <p>Dirección de entrega</p>
-            {existsAddress && isLoggedIn ?
-                <input type="text" className="address" value={address} onChange={(e) => setAddress(e.target.value)} ref={inputRef}></input> :
-                <input type="text" placeholder="Dirección de entrega" className="address" value={address} onChange={(e) => setAddress(e.target.value)} ref={inputRef}></input>}
-            <div className="cart-actions">
-                <button onClick={clearOrder} className="clear-order">Vaciar carrito</button>
-                <button onClick={() => alert("Pedido confirmado")} disabled={isLoggedIn && !error ? false : true} className={isLoggedIn && !error ? "active-button" : "disabled-button"}>Confirmar pedido</button>
+            <div className="section-address">
+                <div>
+                    <p>Dirección de entrega</p>
+                    {existsAddress && isLoggedIn ?
+                        <input type="text" className="address" value={address} onChange={(e) => setAddress(e.target.value)} ref={inputRef} name="address"></input> :
+                        <input type="text" placeholder="Dirección de entrega" className="address" value={address} onChange={(e) => setAddress(e.target.value)} ref={inputRef} name="address"></input>}
+                </div>
+                <div>
+                    <p>Información adicional</p>
+                    <input type="text" placeholder="Puerta, piso ..." className="address" name="information" value={extraInformation} onChange={(e) => setExtraInformation(e.target.value)}/>
+                </div>
+
+            </div>
+            <div className="buttons-sections">
+                <div className="payment">
+                    <button onClick={() => handlePayment("tarjeta")} className={selectedPayment === "tarjeta" ? "selected" : ""}>Tarjeta</button>
+                    <button onClick={() => handlePayment("efectivo")} className={selectedPayment === "efectivo" ? "selected" : ""}>Efectivo</button>
+                </div>
+                <div className="cart-actions">
+                    <button onClick={clearOrder} className="clear-order">Vaciar carrito</button>
+                    <button onClick={submitOrder} disabled={isLoggedIn && !error ? false : true} className={isLoggedIn && !error ? "active-button" : "disabled-button"}>Confirmar pedido</button>
+                </div>
             </div>
             {isLoggedIn ? null : <p className="login-warning">Para confimar el pedido, primero debe iniciar sesión</p>}
             {error && <ErrorMessage error={error} />}
